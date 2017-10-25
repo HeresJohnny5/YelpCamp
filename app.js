02503bdccc53b67b642dff2bdfd5a20bb198d3f6
John Erickson <ericksonjohn08@hotmail.com>
2017-10-24 21:44:59 -0400

    Remove styles from app. Add bootstrap content to campgrounds view. Add header-footer partials to new and landing view.

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.set('view engine', 'ejs');
 var campgrounds = [
 		{ name: "Crater Lake", image: "https://images.unsplash.com/photo-1437422061949-f6efbde0a471?w=2550&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" },
 		{ name: "Mount Hood", image: "https://images.unsplash.com/photo-1431512284068-4c4002298068?w=2552&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" },
+		{ name: "Three Sisters", image: "https://images.unsplash.com/photo-1500534623283-312aade485b7?w=2550&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" },
+		{ name: "Crater Lake", image: "https://images.unsplash.com/photo-1437422061949-f6efbde0a471?w=2550&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" },
+		{ name: "Mount Hood", image: "https://images.unsplash.com/photo-1431512284068-4c4002298068?w=2552&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" },
 		{ name: "Three Sisters", image: "https://images.unsplash.com/photo-1500534623283-312aade485b7?w=2550&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" }
 ];
 
