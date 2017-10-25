6c44c5ea4d7b7e96be7f23907e4c2e9f0c5f3f93
John Erickson <ericksonjohn08@hotmail.com>
2017-10-24 21:14:36 -0400

    Made campgrounds array global. Add campgrounds post and new view. Add app.css

--- app.js
+++ app.js
@@ -6,17 +6,31 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+var campgrounds = [
+		{ name: "Crater Lake", image: "https://images.unsplash.com/photo-1437422061949-f6efbde0a471?w=2550&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" },
+		{ name: "Mount Hood", image: "https://images.unsplash.com/photo-1431512284068-4c4002298068?w=2552&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" },
+		{ name: "Three Sisters", image: "https://images.unsplash.com/photo-1500534623283-312aade485b7?w=2550&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" }
+];
+
 app.get('/', function(req, res) {
 	res.render('landing');
 });
 
 app.get('/campgrounds', function(req, res) {
-	var campgrounds = [
-		{ name: "Crater Lake", image: "https://images.unsplash.com/photo-1437422061949-f6efbde0a471?w=2550&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" },
-		{ name: "Mount Hood", image: "https://images.unsplash.com/photo-1431512284068-4c4002298068?w=2552&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" },
-		{ name: "Three Sisters", image: "https://images.unsplash.com/photo-1500534623283-312aade485b7?w=2550&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" }
-	];
 	res.render('campgrounds', { campgrounds: campgrounds });
+});
+
+app.post('/campgrounds', function(req, res) {
+    var campground = req.body.campground;
+    var image = req.body.image;
+    var newCampground = { name: campground, image: image };
+    
+    campgrounds.push(newCampground);
+    res.redirect('campgrounds');
+});
+
+app.get('/campgrounds/new', function(req, res) {
+    res.render('new');
 });
 
 app.listen(3000, function () {
